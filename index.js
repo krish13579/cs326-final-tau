d926ff6d1727d335d977cf160fb0e42e5da4f26e
maahigoel@umass.edu <maahigoel@umass.edu>
2022-11-08 17:26:27 -0500

    updated ejs

--- index.js
+++ index.js
@@ -6,9 +6,6 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/', function(req, res) {
-    res.send("Hey there! Thanks for visting the site! Be sure to <a href='/views/pages/login Page/login.html'>login</a>!");
-    })
+  .get('/', (req, res) => res.render('pages/login Page/login.ejs'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
   
