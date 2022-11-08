6949aef0a229fbcd91279d4f39b158d85d1bfb38
maahigoel@umass.edu <maahigoel@umass.edu>
2022-11-07 23:02:05 -0500

    linked

--- views/pages/LogIn Page/login.js
+++ views/pages/LogIn Page/login.js
@@ -1,32 +1,36 @@
 
 document.getElementById("submit").addEventListener("click", () => {
-    if(isExistingUser()){
+    if (isExistingUser()) {
         window.location.href = "../Find Riders Page/findRiders.html";
-    }});
+    }
+});
 
-function isExistingUser(){
+document.getElementById("signup").addEventListener("click", () => {
+    window.location.href = "../Sign Up Page/signUp.html";
+});
+function isExistingUser() {
     let username = document.getElementById("Username").value;
     let password = document.getElementById("Password").value;
 
-    if(username.length === 0 || password.length === 0){
+    if (username.length === 0 || password.length === 0) {
         alert("Please enter a username and password to login");
         return false;
     }
-   
+
     const usersList = verify();
 
-    if(usersList.hasOwnProperty(username) && usersList[username]==password){
+    if (usersList.hasOwnProperty(username) && usersList[username] == password) {
         // alert("SUCCESS");
         return true;
     }
-    else{
+    else {
         alert("Invalid username or password. Please try again or sign up!")
         return true;
     }
-    
-    
+
+
 }
-function verify(){
+function verify() {
     // let response = await fetch(url);
     // if(response.ok){
     //     let response2 = await response.json();
@@ -34,7 +38,7 @@ function verify(){
     // }else{
     //     alert("error getting data from server")
     // }
-    let users = {"maahig": "password", "krishM": "password123", "kabirI": "password0"};
+    let users = { "maahig": "password", "krishM": "password123", "kabirI": "password0" };
     return users;
 
 }
