c7eca0eee7ec65b4b0b2bdddcf0a09e99a13c251
krish13579 <113211385+krish13579@users.noreply.github.com>
2022-11-09 00:51:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
@@ -12,12 +13,18 @@ const app = express()
 
 
 
-  router.get('/views/pages/logIn%20Page/login', (req, res) => res.render('pages/logIn Page/login'))
-  router.get('/views/pages/Your%20Rides%20Page/yourRides', (req, res) => res.render('pages/Your Rides Page/yourRides'))
-  router.get('/views/pages/Sign%20Up%20Page/signUp', (req, res) => res.render('pages/Sign Up Page/signUp'))
-  router.get('/views/pages/Your%20Profile%20Page/profile', (req, res) => res.render('pages/Your Profile Page/profile'))
-  router.get('/views/pages/Find%20Riders%20Page/findRiders', (req, res) => res.render('pages/Find Riders Page/findRiders'))
-  router.get('/', (req, res) => res.render('pages/index'))
-  router.get('/views/pages/Find%20Drivers%20Page/findDrivers', (req, res) => res.render('pages/Find Drivers Page/findDrivers'))
-  router.get('/views/pages/Messages%20Page/messages', (req, res) => res.render('pages/Messages Page/messages'))
+  app.get('/views/pages/logIn%20Page/login', (req, res) => res.render('pages/logIn Page/login'))
+  app.get('/views/pages/Your%20Rides%20Page/yourRides', (req, res) => res.render('pages/Your Rides Page/yourRides'))
+  app.get('/views/pages/Sign%20Up%20Page/signUp', (req, res) => res.render('pages/Sign Up Page/signUp'))
+  app.get('/views/pages/Your%20Profile%20Page/profile', (req, res) => res.render('pages/Your Profile Page/profile'))
+  app.get('/views/pages/Find%20Riders%20Page/findRiders', (req, res) => res.render('pages/Find Riders Page/findRiders'))
+  app.get('/', (req, res) => res.render('pages/index'))
+  app.get('/views/pages/Find%20Drivers%20Page/findDrivers', (req, res) => res.render('pages/Find Drivers Page/findDrivers'))
+  app.get('/views/pages/Messages%20Page/messages', (req, res) => res.render('pages/Messages Page/messages'))
   app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+  router.post("/createUser", (req, res) => {
+    console.log(req);
+    res.send("user created ");
+  });
