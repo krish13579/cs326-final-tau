d2c74c2378ba62bc9cfeaac2376c3c565edcf1b6
maahigoel@umass.edu <maahigoel@umass.edu>
2022-11-08 19:35:01 -0500

    langing pade updated

--- index.js
+++ index.js
@@ -8,6 +8,5 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  // .get('/', (req, res) => res.render('views/pages/logIn Page/login')) 
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
   
