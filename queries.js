dd5518fad3093cd283fec419b1d5c19f04b90178
maahigoel@umass.edu <maahigoel@umass.edu>
2022-11-16 19:27:25 -0500

    test

--- queries.js
+++ queries.js
@@ -1,11 +1,15 @@
 const Pool = require('pg').Pool
+require('dotenv').config()
 const pool = new Pool({
-  user: 'vsxcthxiaisgiy',
-  host: 'ec2-18-215-41-121.compute-1.amazonaws.com',
-  database: 'depqr2p28kevvk',
-  password: 'f9c54a39e2e9d24873f2c2ae11bb997a15f19e1bd12e4d2818fc2e7606c37843',
-  port: 5432,
-})
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+  port: process.env.POSTGRES_PORT,
+  ssl: {
+      rejectUnauthorized: false,
+  }
+});
 
 const getAllRequestedRides = (request, response) => {
   pool.query(`SELECT origin, destination, date, price, seats from rides where type='requested'`, (error, results) => {
