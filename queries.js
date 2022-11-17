d8671dfeb6df081e5b74b4528ee59c1344d4cd06
maahigoel@umass.edu <maahigoel@umass.edu>
2022-11-16 19:23:31 -0500

    test

--- queries.js
+++ queries.js
@@ -1,7 +1,7 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
   user: 'vsxcthxiaisgiy',
-  host: 'c2-18-215-41-121.compute-1.amazonaws.com',
+  host: 'ec2-18-215-41-121.compute-1.amazonaws.com',
   database: 'depqr2p28kevvk',
   password: 'f9c54a39e2e9d24873f2c2ae11bb997a15f19e1bd12e4d2818fc2e7606c37843',
   port: 5432,
