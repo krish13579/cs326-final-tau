f8741574d390700ebf1a742bfdba5af20c7ac929
Kabir Israni <kisrani@umass.edu>
2022-11-20 12:56:36 -0500

    Session Libraries Added

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const db = require('./queries')
+const cookieParser = require("cookie-parser");
+const sessions = require('express-session');
 
 
 
