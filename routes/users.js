dc32afdad7b85c69997c17e143fc903413bde7c1
maahigoel@umass.edu <maahigoel@umass.edu>
2022-12-06 21:02:57 -0500

    signup db connected

--- routes/users.js
+++ routes/users.js
@@ -27,7 +27,8 @@ async function encryptAndSaveToDB(newData) {
         const salt = await bcrypt.genSalt();
         const hashedPassWord = await bcrypt.hash(newData.password, salt);
         result.password = hashedPassWord;
-        userObjArray.push(result);
+        return result;
+        //userObjArray.push(result);
     }
     catch {
         console.log("Unable to encrypt and save data");
@@ -45,16 +46,16 @@ async function compareHashedPassword(passInDb, passSent) {
 
 
 
-async function deleteUserFromDB(userID){
+async function deleteUserFromDB(userID) {
     const id = userID
-  
+
     pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.status(200).send(`User deleted with ID: ${id}`)
+        if (error) {
+            throw error
+        }
+        response.status(200).send(`User deleted with ID: ${id}`)
     })
-  }
+}
 
 
 //Sign Up Routes
@@ -69,34 +70,64 @@ router.get("/verify/:userObj", async (req, res) => {
 
     const passdata = await pool.query(`SELECT password from users where users.email=$1;`, [userObj.email])
     const arr = passdata.rows;
-    console.log(arr)
-    console.log((arr[0]))
     if (arr.length != 0) {
         foundObj.status = true;
-        foundObj.password = arr[0];
+        foundObj.password = JSON.stringify(arr[0].password);
     }
-    // console.log("test" , compareHashedPassword(foundObj.password, userObj.password))
+    console.log("found: " + foundObj.password)
+    console.log("usrer: " + userObj.password)
+    console.log(await compareHashedPassword(foundObj.password, userObj.password))
+
     const response = {
         "validity": await compareHashedPassword(foundObj.password, userObj.password),
-        "comments": foundObj.status === false ? "No Account In Database" : compareHashedPassword(foundObj.password, userObj.password) ? "Account Exists" : "Incorrect Password"
+        "comments": foundObj.status === false ? "No Account In Database" : await compareHashedPassword(foundObj.password, userObj.password) ? "Account Exists" : "Incorrect Password"
     };
     console.log(response)
 
     res.status(200).json(response);
 });
 
+//register new user
+router.get("/register/:userObj", async (req, res) => {
+    const userObj = JSON.parse(req.params.userObj);
+    
+    const fname = userObj.firstName
+    const lname = userObj.lastName
+    const bdate = userObj.birthday
+    const email = userObj.email
+    const password = userObj.password
+
+    const data = await pool.query(`SELECT * FROM users WHERE users.email= $1;`, [userObj.email]); //Checking if user already exists
+    const arr = data.rows;
+    if (arr.length != 0) {
+        res.status(401).send('User exists, please log in.');
+    }
+    else {
+        const hashedUser = await encryptAndSaveToDB(userObj)
+        console.log(hashedUser.password)
+        pool.query('INSERT INTO users (fname, lname, bdate, email, password) VALUES ($1, $2, $3, $4, $5)', [userObj.firstName, userObj.lastName, userObj.birthday, userObj.email, hashedUser.password], (error, results) => {
+            if (error) {
+                throw error;
+            }
+            res.status(201).json({ status: 'success', message: `User added with ID: ${results.email}` });
+
+        })
+    }
+
+
+});
 //Individual User Routes
 router.route("/:id")
-    .get((req, res) => {
+    .get(async (req, res) => {
         const userId = req.params.id;
         let result = {};
-        userObjArray.forEach(x => {
-            if (x.email === userId) {
-                result["firstName"] = x.firstName;
-                result["lastName"] = x.lastName;
-                result["birthday"] = x.birthday;
-            };
-        });
+        const user = await pool.query(`SELECT * from users where users.email=$1;`, [userId])
+        const data = user.rows;
+        if (data.length != 0) {
+            result["firstName"] = data[0].fname;
+            result["lastName"] = data[0].lname;
+            result["birthday"] = data[0].bdate;
+        }
         res.status(200).json(result);
     })
     .post((req, res) => {
@@ -109,7 +140,7 @@ router.route("/:id")
         res.sendStatus(200);
     })
     .put((req, res) => {
-        console.log(userObjArray);
+        // console.log(userObjArray);
         const userId = req.params.id;
         //Removed Old User Data
         userObjArray = JSON.parse(JSON.stringify(deleteUserFromDB(userId)));
