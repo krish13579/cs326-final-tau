13e19956a24a5f24be581ea716c45997a569aba8
krish13579 <113211385+krish13579@users.noreply.github.com>
2022-12-06 19:12:03 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const PORT = process.env.PORT || 5000
 const db = require('./queries');
 //Set Up View Engine
 app.set("view engine", "ejs");
- 
-//CSS
 app.use('/public', express.static('public'));
 
 //Root Route
@@ -18,6 +16,24 @@ app.get("/yourProfile/:userInfo", (req, res) => {
     res.render("profile", JSON.parse(req.params.userInfo));
 });
 
+app.get("/findDrivers/:userInfo", (req, res) => {
+  
+    res.render("findDrivers",JSON.parse(req.params.userInfo));
+});
+
+app.get("/findRiders/:userInfo",(req, res) => {
+    
+    res.render("findRiders", JSON.parse(req.params.userInfo));
+});
+
+app.get("/yourRides/:userInfo",(req, res) => {
+    
+    res.render("yourRides", JSON.parse(req.params.userInfo));
+});
+
+
+
+
 //Sign Up Route
 app.get("/signUp", (req, res) => {
     res.render("signUp");
@@ -26,5 +42,10 @@ app.get("/signUp", (req, res) => {
 //User Router
 const userRouter = require("./routes/users");
 app.use("/users", userRouter);
+
+//Rides Rouetr
+const ridesRouter = require("./routes/rides");
+app.use("/rides",ridesRouter);
+
 
 app.listen(PORT);
