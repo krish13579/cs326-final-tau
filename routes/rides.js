7460e94f31396cfce058c9a6d0404c1a9575af6b
Kabir Israni <kisrani@umass.edu>
2022-12-11 23:15:01 -0500

    push

--- routes/rides.js
+++ routes/rides.js
@@ -53,7 +53,8 @@ router.get("/getBookedRides/:userInfo", (req, res) => {
 //Query db to get all rides a user has created
 router.get("/getOfferedRides/:userInfo", (req, res) => {
     let user = req.params.userInfo;
-    pool.query(`SELECT rideid, origin, destination, to_char(date, 'Mon/DD/YYYY') date, price from rides where $1=ANY(bookedusers)`, [user], (error, results) => {
+    user = JSON.parse(user);
+    pool.query(`SELECT rideid, origin, destination, to_char(date, 'Mon/DD/YYYY') date, price from rides where rides.type = 'offered' and rides.creator = $1`, [user], (error, results) => {
         if (error) {
             throw error
         }
